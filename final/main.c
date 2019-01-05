6b6eed03a357760cfec5656d6e7ee9ccf1268a99
慕 <dj.snowman11@gmail.com>
2019-01-05 21:25:57 +0800

    完成0路徑(土炮)，終點變更顏色，計算最短路徑

--- final/main.c
+++ final/main.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-#define size 50
+#define size 20
 
-void print(int );
-void clear(void);
+void print(int ,int);
+void clear(int );
 void random_number(int );
 void print_test(int );
-int from_to(int ,int ,int ,int ,int );
-int data[size*2+1][size*2+1];
+void from_to(int ,int ,int ,int ,int ,int ,int );
+void d_bug(int );
+
+int data[size*2+1][size*2+1],data_reg[size*2+1][size*2+1];
+int ans,ans_reg;
 
 int main(int argc, char *argv[]) {
+	srand(time(NULL));
 	int size_in;
 	int x_from,y_from;
 	int x_to,y_to;
-	clear();
+	int cmd;
+	clear(1);
 	do{
-		printf("請輸入矩陣大小(Max=50):");
+		printf("請輸入矩陣大小(Max=%d):",size);
 		scanf("%d",&size_in);
 		if(size_in<=0 || size_in>50){
 			printf("輸入矩陣大小erro\n");
 		}
 	}while(size_in<=0 || size_in>50);
 	random_number(size_in);
-	print(size_in);
+	d_bug(size_in);
+	print(size_in,0);
+	//print_test(size_in);
 	do{
 		printf("請輸入起點(x,y):");
 		scanf("%d %d",&x_from,&y_from);
@@ -40,74 +48,116 @@ int main(int argc, char *argv[]) {
 		}
 	}while(x_to<0 || x_to>=size_in || y_to<0 || y_to>=size_in);
 	
+	ans_reg = size_in*size_in;
 	data[y_from*2+1][x_from*2+1]=2;
-	if(from_to(x_from*2+1,y_from*2+1,size_in,x_to,y_to)==2){
+	ans = 0;
+	from_to(x_from*2+1,y_from*2+1,size_in,x_to,y_to,2,0);
+	
+	while(1){
+		do{
+			printf("1:顯示全部路徑  2:顯示最佳路徑  3:結束\n");
+			scanf("%d",&cmd);
+			if(cmd<1 || cmd>3){
+				printf("輸入錯誤\n");
+			}
+		}while(cmd<1 || cmd>3);
+		if(cmd==3){
+			break;
+		}
+		else if(cmd==1){
+			data[y_from*2+1][x_from*2+1]=2;
+			ans = 0;
+			from_to(x_from*2+1,y_from*2+1,size_in,x_to,y_to,1,0);
+		}
+		else if(cmd==2){
+			print(size_in,1);
+			printf("最佳路徑步數%d\n",ans_reg);
+		}
+	}
+	if(ans==0){
 		printf("00000000");
 	}
-	//print(size_in);
-//	print_test(size_in);
-	system("pause");
+	//print_test(size_in);
+	//system("pause");
 	return 0;
 }
 
-int from_to(int x,int y,int size_in,int x_to,int y_to){
-	system("cls");
-	print(size_in);
-	//system("pause");
+void from_to(int x,int y,int size_in,int x_to,int y_to,int cmd,int step){
+	if(cmd==0 && ans!=0){
+		return;
+	}
+	if(cmd == 1){
+		system("cls");
+		print(size_in,0);
+	}
 	if(x==x_to*2+1 && y==y_to*2+1){
-		system("pause");
-		return 1;
+		if(cmd == 1){
+			printf("此路徑步數%d，最佳路徑步數%d\n",step,ans_reg);
+			system("pause");
+		}
+		else if(cmd==2){
+			if(step<ans_reg){
+				ans_reg = step;
+				memcpy(data_reg, data, sizeof(data_reg));
+			}
+		}
+		ans++;
+		return;
 	}
 	if(x>0){
 		if(data[y][x+1]==0 && data[y][x+2] != 2){
-			data[y][x+2] = 2;
-			from_to(x+2,y,size_in,x_to,y_to);
+			if(x+2==x_to*2+1 && y==y_to*2+1){
+				data[y][x+2] = 4;
+			}
+			else{
+				data[y][x+2] = 2;
+			} 
+			from_to(x+2,y,size_in,x_to,y_to,cmd,step+1);
 			data[y][x+2] = 3;
 		}
 	}
 	if(x<size_in*2+1){
 		if(data[y][x-1]==0 && data[y][x-2] != 2){
-			data[y][x-2] = 2;
-			from_to(x-2,y,size_in,x_to,y_to);
+			if(x-2==x_to*2+1 && y==y_to*2+1){
+				data[y][x-2] = 4;
+			}
+			else{
+				data[y][x-2] = 2;
+			}
+			from_to(x-2,y,size_in,x_to,y_to,cmd,step+1);
 			data[y][x-2] = 3;
 		}
 	}
 	if(y>0){
 		if(data[y+1][x]==0 && data[y+2][x] != 2){
-			data[y+2][x] = 2;
-			from_to(x,y+2,size_in,x_to,y_to);
+			if(x==x_to*2+1 && y+2==y_to*2+1){
+				data[y+2][x] = 4;
+			}
+			else{
+				data[y+2][x] = 2;
+			}
+			from_to(x,y+2,size_in,x_to,y_to,cmd,step+1);
 			data[y+2][x] = 3;
 		}
 	}
 	if(y<size_in*2+1){
 		if(data[y-1][x]==0 && data[y-2][x] != 2){
-			data[y-2][x] = 2;
-			from_to(x,y-2,size_in,x_to,y_to);
+			if(x==x_to*2+1 && y-2==y_to*2+1){
+				data[y-2][x] = 4;
+			}
+			else{
+				data[y-2][x] = 2;
+			}
+			from_to(x,y-2,size_in,x_to,y_to,cmd,step+1);
 			data[y-2][x] = 3;
 		}
 	}
-	return 2;
+	return;
 }
 
 void random_number(int size_in){
-	srand(time(NULL));
-	int x,y,ans;
-	for(y=0;y<size_in*2+1;y++){
-		for(x=size_in*2;x>=0;x--){
-			if((x!=0 && y!=0) && (x!=size_in*2 && y!=size_in*2)){
-				if((x%2==0 || y%2==0) && !(x%2==0 && y%2==0)){
-					ans = rand()%4;
-					if(ans == 0 || ans == 1|| ans == 2){
-						data[y][x] = 0;
-					}
-				}
-			}
-		}
-	}
-}
-
-/*void random_number(int size_in){
-	srand(time(NULL));
+	//srand(time(NULL));
+	clear(0);
 	int x,y,ans;
 	for(y=0;y<size_in*2+1;y++){
 		for(x=0;x<size_in*2+1;x++){
@@ -122,15 +172,34 @@ void random_number(int size_in){
 		}
 	}
 }
-*/ 
-void print(int size_in){
+void d_bug(int size_in){
+	printf("系統忙線中，請稍後...");
+	do{
+		srand(time(NULL));
+		do{
+			ans = 0;
+			from_to(1,1,size_in,size_in-1,size_in-1,0,0);
+			if(ans==0){
+				random_number(size_in);
+			}
+		}while(ans==0);
+		ans = 0;
+		from_to(1,size_in*2-1,size_in,size_in-1,1,0,0);
+		if(ans==0){
+			random_number(size_in);
+		}
+	}while(ans==0);
+	system("cls");
+}
+
+void print(int size_in,int cmd){
 
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	WORD saved_sttributes;
 	CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
 	GetConsoleScreenBufferInfo(hConsole, &csbiInfo);
 	saved_sttributes = csbiInfo.wAttributes;
-	
+	system("cls");
 	int x,y;
 	printf("y\\x");
 	for(x=size_in-1;x>=0;x--){
@@ -139,13 +208,17 @@ void print(int size_in){
 	printf("\n");
 	for(y=0;y<size_in*2+1;y++){
 		for(x=size_in*2;x>=0;x--){
-			if(data[y][x]==2){
+			if((cmd==0)?data[y][x]==2 : data_reg[y][x]==2){
+				SetConsoleTextAttribute(hConsole,FOREGROUND_GREEN);
+				printf("@");
+				SetConsoleTextAttribute(hConsole,saved_sttributes);
+			}
+			else if((cmd==0)?data[y][x]==4 : data_reg[y][x]==4){
 				SetConsoleTextAttribute(hConsole,FOREGROUND_RED);
 				printf("@");
 				SetConsoleTextAttribute(hConsole,saved_sttributes);
-				//printf(" ");
 			}
-			else if(data[y][x]==1){
+			else if((cmd==0)?data[y][x]==1 : data_reg[y][x]==1){
 				if(y==0){
 					if(x==0){
 						printf("╗ ");
@@ -196,11 +269,11 @@ void print(int size_in){
 						printf("║  ");
 					}
 					else{
-						printf("  ");
+						printf("　");
 					}
 				}
 			}
-			else if(data[y][x]==3){
+			else if((cmd==0)?data[y][x]==3 : data_reg[y][x]==3){
 				printf(" ");
 			}
 			else{
@@ -211,15 +284,25 @@ void print(int size_in){
 	}
 }
 
-void clear(void){
+void clear(int cmd){
 	int x,y;
 	for(y=0;y<size*2+1;y++){
 		for(x=size*2;x>=0;x--){
 			if(y%2==1 && x%2==1){
-				data[y][x] = 3;
+				if(cmd==0){
+					data[y][x] = 3;
+				}
+				else{
+					data_reg[y][x] = 3;
+				}
 			}
 			else{
-				data[y][x] = 1;
+				if(cmd==0){
+					data[y][x] = 1;
+				}
+				else{
+					data_reg[y][x] = 1;
+				}
 			}
 		}
 	}
@@ -234,3 +317,133 @@ void print_test(int size_in){
 		printf("\n");
 	}
 }
+
+/*void d_bug(int x,int y,int size_in){
+	srand(time(NULL));
+	int reg;
+	if(data[y][x+1]==1 && data[y][x-1]==1 && data[y+1][x]==1 && data[y-1][x]==1){
+		if(x==1 && y==1){
+			reg = rand()%2;
+			if(reg==0){
+				data[y][x+1] = 0;
+			}
+			else if(reg==1){
+				data[y+1][x] = 0;
+			}
+		}
+		else if(x==1 && y==size_in*2){
+			reg = rand()%2;
+			if(reg==0){
+				data[y][x+1] = 0;
+			}
+			else if(reg==1){
+				data[y-1][x] = 0;
+			}
+		}
+		else if(x==size_in*2 && y==1){
+			reg = rand()%2;
+			if(reg==0){
+				data[y][x-1] = 0;
+			}
+			else if(reg==1){
+				data[y+1][x] = 0;
+			}
+		}
+		else if(x==size_in*2 && y==size_in*2){
+			reg = rand()%2;
+			if(reg==0){
+				data[y][x-1] = 0;
+			}
+			else if(reg==1){
+				data[y-1][x] = 0;
+			}
+		}
+		else if(x==1){
+			reg = rand()%3;
+			if(reg==0){
+				data[y][x+1] = 0;
+			}
+			else if(reg==1){
+				data[y-1][x] = 0;
+			}
+			else if(reg==2){
+				data[y+1][x] = 0;
+			}
+		}
+		else if(x==size_in*2){
+			reg = rand()%3;
+			if(reg==0){
+				data[y][x-1] = 0;
+			}
+			else if(reg==1){
+				data[y-1][x] = 0;
+			}
+			else if(reg==2){
+				data[y+1][x] = 0;
+			}
+		}
+		else if(y==size_in*2){
+			reg = rand()%3;
+			if(reg==0){
+				data[y][x+1] = 0;
+			}
+			else if(reg==1){
+				data[y][x-1] = 0;
+			}
+			else if(reg==2){
+				data[y-1][x] = 0;
+			}
+		}
+		else if(y==1){
+			reg = rand()%3;
+			if(reg==0){
+				data[y][x+1] = 0;
+			}
+			else if(reg==1){
+				data[y][x-1] = 0;
+			}
+			else if(reg==2){
+				data[y+1][x] = 0;
+			}
+		}
+		else{
+			reg = rand()%4;
+			if(reg==0){
+				data[y][x+1] = 0;
+			}
+			else if(reg==1){
+				data[y][x-1] = 0;
+			}
+			else if(reg==2){
+				data[y+1][x] = 0;
+			}
+			else if(reg==3){
+				data[y-1][x] = 0;
+			}
+		}
+	}
+	
+}
+*/
+
+/*void random_number(int size_in){
+	srand(time(NULL));
+	int x,y,ans;
+	for(y=0;y<size_in*2+1;y++){
+		for(x=size_in*2;x>=0;x--){
+			if((x!=0 && y!=0) && (x!=size_in*2 && y!=size_in*2)){
+				if((x%2==0 && y%2==1)){
+					if(rand()%2 == 0){
+						data[y][x] = 0;
+						data[y+1][x-1] = 1;
+					}
+					else{
+						data[y][x] = 1;
+						data[y+1][x-1] = 0;
+					}
+				}
+			}
+		}
+	}
+}*/
+
